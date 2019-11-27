54f66833174641715aef10487969dbf0e3e99903
Sandi <sandi.c.gau@gmail.com>
2019-11-26 21:35:30 -0600

    Add more routes

--- routes/api/findersFee.js
+++ routes/api/findersFee.js
@@ -2,14 +2,20 @@ const mongoose = require("mongoose");
 const Items = mongoose.model("Items");
 const router = require('express').Router();
 
-//Get items by category
+// Get all items
+router.get("/getAll", async (req, res) => {
+    const results = await Items.find({}).exec({})
+    res.status(200).json(results)
+});
+
+// Get items by category
 router.get("/searchbycategory/:category", async (req, res) => {
     const category = req.params.category
     const results = await Items.find({ itemCategory: category }).exec({})
     res.status(200).json(results)
 });
 
-//Get items by user
+// Get items by user
 router.get("/searchbyuser/:userId", async (req, res) => {
     const userId = req.params.userId
     const results = await Items.find({ userId: userId }).exec({})
@@ -17,38 +23,38 @@ router.get("/searchbyuser/:userId", async (req, res) => {
 });
 
 
-// add new item
+// Add new item
 router.post("/addItem", async (req, res) => {
     const item = new Items(req.body);
     await item.save();
     res.status(200).json(item);
 })
 
-//Set item to found and input finderEmail
+// Set item to found and input finderEmail
 router.put("/updateItemFound", async (req, res) => {
     const updateItem = await Items.findByIdAndUpdate({
         _id: req.body.id
     }, {
-            $set: { itemFound: true },
-            $addToSet: { finderEmail: req.body.finderEmail }
+        $set: { itemFound: true },
+        $addToSet: { finderEmail: req.body.finderEmail }
 
-        }, { new: true }, (err, doc) => {
-            if (err) {
-                console.log(err)
-                return err
-            }
-            else {
-                return doc
-            }
+    }, { new: true }, (err, doc) => {
+        if (err) {
+            console.log(err)
+            return err
         }
+        else {
+            return doc
+        }
+    }
     );
     res.status(200).json(updateItem)
 });
 
-//Delete post
+// Delete ietm
 router.put("/api/deleteItem", async (req, res) => {
     console.log(req.body.id)
-    const deleteItem = Findersfee.findByIdAndRemove({
+    const deleteItem = await Findersfee.findByIdAndRemove({
         _id: req.body.params.id
     }, (err, doc) => {
         if (err) {
